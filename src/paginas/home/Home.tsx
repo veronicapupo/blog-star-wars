99581a46e59905b8794edff5b9d808c33738e4c1
veronica pupo <veronica.pupo@hotmail.com>
2022-09-28 11:03:49 -0300

    inserindo home css

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+
+function Home() {
+return (
+    <>
+        <h1 className= "titulo">Home</h1>
+        <img src="https://lumiere-a.akamaihd.net/v1/images/cg_starwars_skywalkersaga_mobile_20370_7b5e45d9.jpeg?region=0,20,640,360" alt="Imagem Tela Inicial" className="img"/>
+    </>
+);
+}
+export default Home;
