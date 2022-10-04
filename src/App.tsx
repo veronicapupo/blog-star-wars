49d02e6240e3d4cd83fc209d2960eeb1491942dc
veronica pupo <veronica.pupo@hotmail.com>
2022-10-04 15:13:39 -0300

    criando conexão com backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/estaticos/navbar/Navbar';
-import Footer from './components/estaticos/footer/Footer';
-import Home from './paginas/home/Home';
-import { BrowserRouter, Route, Routes}from 'react-router-dom';
-import Login from './paginas/login/Login';
-
+import React from "react";
+import "./App.css";
+import Navbar from "./components/estaticos/navbar/Navbar";
+import Footer from "./components/estaticos/footer/Footer";
+import Home from "./paginas/home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./paginas/login/Login";
+import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
 
 function App() {
-  return(
+  return (
     <BrowserRouter>
-    <Navbar />
-    <div style={{ minHeight: '100vh' }}>
-    <Routes>
-    <Route path="/" element={<Login />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/home" element={<Home />} />
-    45
-    </Routes>
-    </div>
-    <Footer />
-    </ BrowserRouter >
-    );
+      <Navbar />
+      <div style={{ minHeight: "100vh" }}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cadastroUsuario" element={<CadastroUsuario />} />
+          
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 export default App;
---
+++ src/model/User.ts
@@ -0,0 +1,9 @@
+interface User {
+    id: number;
+    foto: string;
+    nome: string;
+    senha: string;
+    usuario: string;
+    
+}
+export default User;
